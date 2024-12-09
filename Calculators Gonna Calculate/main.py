dd319f414268413efce31da97a79184dbb893f66
Anna Prokofeva <boiling.sun@icloud.com>
2024-12-09 20:50:54 +0300

    Refactor create_buttons function to accept global variables as arguments

--- Calculators Gonna Calculate/main.py
+++ Calculators Gonna Calculate/main.py
@@ -1,9 +1,14 @@
 import windows_and_buttons as gui
+import configs
+
 
 def main():
     root, top_frame, mid_frame, bottom_frame = gui.create_window()
     gui.create_display_label(top_frame)
-    gui.create_buttons(bottom_frame)
+    gui.create_buttons(bottom_frame, 
+                       configs.GRID_ROWS, 
+                       configs.GRID_COLUMNS, 
+                       configs.BUTTONS_GRID_KWARGS)
     root.mainloop()
 
 if __name__ == '__main__':
