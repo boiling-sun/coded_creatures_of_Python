a3f445cd2cffbc2f55ac79d8645d83b631841be4
Anna Prokofeva <boiling.sun@icloud.com>
2024-12-25 22:45:05 +0300

    added functions arguments

--- Calculators Gonna Calculate/main.py
+++ Calculators Gonna Calculate/main.py
@@ -2,16 +2,21 @@ import windows_and_buttons as gui
 import configs
 from operations import Calculator
 
-
-
 def main():
     calculator = Calculator()
-    root, top_frame, mid_frame, bottom_frame = gui.create_window(configs.PACK_KWARGS)
+    root, top_frame, mid_frame, bottom_frame = gui.create_window(
+        configs.WINDOW_TITLE, configs.WINDOW_WIDTH, configs.WINDOW_HEIGHT, configs.PACK_KWARGS
+        )
     gui.create_display_label(top_frame, 
                              calculator, 
                              configs.DISPLAY_LABEL_KWARGS, 
                              configs.PACK_KWARGS)
-    gui.create_buttons(bottom_frame, 
+    gui.create_expression_label(mid_frame, 
+                                calculator, 
+                                configs.EXPRESSION_LABEL_KWARGS, 
+                                configs.PACK_KWARGS)
+    buttons = gui.create_buttons(configs.ALL_BUTTON_NAMES,
+                       bottom_frame, 
                        calculator,
                        configs.GRID_ROWS, 
                        configs.GRID_COLUMNS, 
